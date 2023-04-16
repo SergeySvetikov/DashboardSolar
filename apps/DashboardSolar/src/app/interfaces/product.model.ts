3a76f2cfa93e13ae4b781acfd1c1c3fba6fa881c
Sergey Svetikov <serikomg123@gmail.com>
2023-04-16 12:42:42 +0300

    add all

---
+++ apps/DashboardSolar/src/app/interfaces/product.model.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+  createdAt: string;
+  address: string;
+  isActive: boolean;
+}
---
+++ apps/DashboardSolar/src/app/shared/components/products-item/products-item.component.ts
@@ -0,0 +1,25 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  NgModule,
+} from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { SkeletonModule } from 'primeng/skeleton';
+
+@Component({
+  selector: 'app-products-item',
+  templateUrl: './products-item.component.html',
+  styleUrls: ['./products-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductsItemComponent {
+  @Input() product: any | undefined;
+}
+@NgModule({
+  declarations: [ProductsItemComponent],
+  exports: [ProductsItemComponent],
+  imports: [RouterLink, NgIf, SkeletonModule],
+})
+export class ProductsItemComponentModule {}
